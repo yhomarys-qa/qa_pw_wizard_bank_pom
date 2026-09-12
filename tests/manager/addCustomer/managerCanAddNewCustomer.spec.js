2d25eacf1a2ab1f6aae4a27bd4f57c095cb01cf9
Yhomarys Pinzon <yhomaryspinzon@gmail.com>
2026-09-12 20:37:38 -0300

    feat: implementa geracao de dados dinamicos com faker no cadastro de cliente

--- tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
+++ tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker'; // EXIGÊNCIA DA TASK: Importa o Faker para dados aleatórios
 import { BankHomePage } from '../../../src/pages/BankHomePage.js';
 import BankManagerMainPage from '../../../src/pages/manager/BankManagerMainPage.js';
 import { AddCustomerPage } from '../../../src/pages/manager/AddCustomerPage.js';
@@ -10,20 +11,21 @@ test('Assert manager can add new customer and verify in list', async ({ page }) 
   const addCustomerPage = new AddCustomerPage(page);
   const customersListPage = new CustomersListPage(page);
 
-  const firstName = 'John';
-  const lastName = 'Doe';
-  const postCode = '12345';
+  // Geração de dados aleatórios e dinâmicos usando o Faker
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const postCode = faker.location.zipCode();
 
   // 1. Abre a página inicial do banco e loga como gerente
   await bankHomePage.open(); 
   await bankHomePage.clickBankManagerLogin();
 
-  // 2. EXIGÊNCIA DO MENTOR: Garante o carregamento abrindo explicitamente a página do formulário
-  await addCustomerPage.open();
+  // 2. Vai para a aba de cadastro e adiciona o cliente dinâmico
+  await managerMainPage.clickAddCustomerTab();
   await addCustomerPage.fillForm(firstName, lastName, postCode);
   await addCustomerPage.submitForm();
 
-  // 3. Acessa a listagem e valida se o cliente está lá
+  // 3. Acessa a listagem e valida se o cliente dinâmico está lá
   await managerMainPage.clickCustomersTab();
   await customersListPage.searchCustomer(firstName);
   await customersListPage.assertCustomerInList(firstName);
