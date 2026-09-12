0ab2ebe0d18c3f415504b3f03ab9bd3dcde392ba
Yhomarys Pinzon <yhomaryspinzon@gmail.com>
2026-09-12 19:23:26 -0300

    feat: conclui automacao de moedas e finaliza testes do gerente

--- tests/manager/openAccount/managerCanChooseCurrencies.spec.js
+++ tests/manager/openAccount/managerCanChooseCurrencies.spec.js
@@ -1,8 +1,28 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import { BankHomePage } from '../../../src/pages/BankHomePage.js';
+import BankManagerMainPage from '../../../src/pages/manager/BankManagerMainPage.js';
+import { OpenAccountPage } from '../../../src/pages/manager/OpenAccountPage.js';
 
-test.beforeEach(async ({ page }) => {});
-test('Assert manager can search customer', async ({ page }) => {
-  // Simula o sucesso da busca
+test('Assert manager can choose currencies', async ({ page }) => {
+  const bankHomePage = new BankHomePage(page);
+  const managerMainPage = new BankManagerMainPage(page);
+  const openAccountPage = new OpenAccountPage(page);
+
+  // 1. Abre a página e faz login como gerente
+  await bankHomePage.open(); 
+  await bankHomePage.clickBankManagerLogin();
+
+  // 2. Acessa a tela de abertura de conta
+  await managerMainPage.clickOpenAccountTab();
+
+  // 3. Seleciona um cliente base
+  await openAccountPage.selectCustomer('Hermoine Granger');
+
+  // 4. Valida se consegue alternar entre as moedas disponíveis (Dollar, Pound, Rupee)
+  await openAccountPage.selectCurrency('Dollar');
+  await openAccountPage.selectCurrency('Pound');
+  await openAccountPage.selectCurrency('Rupee');
 });
+
 
 
